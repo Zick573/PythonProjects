f966dc02026ddee13a9a28cd470550aed48dc9b6
Yurii <grumarket@gmail.com>
2021-04-04 17:46:51 +0300

    ver1

---
+++ strings.py
@@ -0,0 +1,8 @@
+import pyperclip
+pyperclip.copy('I\'m swager')
+print(pyperclip.paste())
+
+name,place,time,food = 'Dima','Park','Noon','Burito'
+print('Hello %s you are invited to a party at %s at %s. PLease Brings %s' % (name,place,time,food))
+
+
